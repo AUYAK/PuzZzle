3145ec668bf6ab583755e204ec9d768366985d07
Alex Y <blablabla@somemail.com>
2022-12-14 22:44:30 +0300

    Animation of player is added (idle + movements)

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,19 +3,37 @@ using UnityEngine.SceneManagement;
 
 public class PlayerController : MonoBehaviour
 {
-    private void FixedUpdate() {
+    private Animator anim;
+    private void Start()
+    {
+        anim = GetComponentInChildren<Animator>(); // in children, just to make possible relative position during animation
+    }
+    private void FixedUpdate()
+    {
         if (Input.GetKey(KeyCode.R))
         {
-           SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);  
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
         }
         if (Input.GetKey(KeyCode.N))
         {
-           SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex+1);  
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+        if (GetComponent<Rigidbody>().velocity.magnitude == 0)
+        {
+            anim.SetBool("isMoving", false);
+        }
+        else
+        {
+            anim.SetBool("isMoving", true);
         }
     }
-    private void OnTriggerEnter(Collider other) {
-        if (this.CompareTag("Player") && other.CompareTag("Finish")){
-            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex+1);
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (this.CompareTag("Player") && other.CompareTag("Finish"))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
         }
     }
 }
